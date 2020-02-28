0c273d1a83fb46800824b7d65c17493e49617240
彭双塔 <1120160658@qq.com>
2020-02-28 11:29:19 +0800

    1、上传sdk

---
+++ PST/Frameworks/PST.framework/Headers/PST.h
@@ -0,0 +1,19 @@
+//
+//  PST.h
+//  PST
+//
+//  Created by 彭双塔 on 2020/2/28.
+//  Copyright © 2020 pst. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PST.
+FOUNDATION_EXPORT double PSTVersionNumber;
+
+//! Project version string for PST.
+FOUNDATION_EXPORT const unsigned char PSTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PST/PublicHeader.h>
+
+#import <PST/PST_HelloWorld.h>
---
+++ PST/Frameworks/PST.framework/Headers/PST_HelloWorld.h
@@ -0,0 +1,19 @@
+//
+//  PST_HelloWorld.h
+//  PST
+//
+//  Created by 彭双塔 on 2020/2/28.
+//  Copyright © 2020 pst. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface PST_HelloWorld : NSObject
+ 
++(NSString *)sayHelloWorld;
+
+@end
+
+
